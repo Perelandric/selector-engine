e8c90b08c2bde6dcc3c10ff55022d66f1e303583
Perelandric <perelandric@gmail.com>
2016-10-05 11:41:20 -0500

    update tests

--- test/test.js
+++ test/test.js
@@ -11,39 +11,42 @@ jsdom.env(
   function (err, window) {
     const Query = window.Query
     const doc = window.document
+
     var sel = "div#foo.foo.bar:first-of-type:last-of-type:only-of-type" +
                  ":first-child:last-child:only-child[data-t][data-t*='-']" +
                  "[data-t^=foo][data-t$=bar][data-t|=foo][data-t=foo-bar]" +
                  "[data-t~=foo-bar]:not(p):nth-child(1)"
 
-    Query.log = function() { console.log.apply(console, arguments) }
     const outer = Query.one(doc, "div")
-
-    var div = null
 
     QuickTest({
       name: "All selector tests"
     },
-      t => {
+      function simple_div(t) {
         t.equal(doc.body.firstElementChild, outer)
+      },
 
-        div = outer.firstElementChild
+      function complex_div(t) {
+        t.true(Query.matches(outer.firstElementChild, sel))
+        t.equal(outer.firstElementChild, Query.one(outer, "div"))
+      },
 
-        t.equal(true, Query.matches(div, sel))
-        t.equal(div, Query.one(outer, "div"))
-
+      function span_in_complex_div(t) {
         sel += " span"
 
+        const div = outer.firstElementChild
         const span = div.appendChild(doc.createElement("span"))
 
-        t.equal(true, Query.matches(span, sel))
+        t.true(Query.matches(span, sel))
         t.equal(span, Query.one(outer, sel))
+      },
 
+      function multi_with_p(t) {
         sel += ", p"
 
         const p = outer.insertBefore(doc.createElement("p"), outer.firstChild)
 
-        t.equal(true, Query.matches(p, sel))
+        t.true(Query.matches(p, sel))
         t.equal(p, Query.one(outer, sel))
       }
     )
